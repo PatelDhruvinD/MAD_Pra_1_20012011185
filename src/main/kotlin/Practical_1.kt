ef36c8c1ea9c47679cc34d49cc2724c606bad6cf
Malay-Thakkar <78149426+Malay-Thakkar@users.noreply.github.com>
2022-08-10 10:28:49 +0530

    ADD some practical exercise

--- src/main/kotlin/Practical_1.kt
+++ src/main/kotlin/Practical_1.kt
@@ -1,5 +1,9 @@
+fun Fact(no: Int):Long{
+    return if(no==1) no.toLong()        // terminate condition
+    else no*Fact(no-1)
+}
 fun main() {
-    println("Hello World!")
+   println("Hello World!")
 
     println("********** Practical = 1_1 **********")
 
@@ -107,5 +111,29 @@ fun main() {
         println("$monthno is December")
     else
         println("$monthno is not valid no")
+
+    println("********** Practical = 1_6 **********")
+    println("Enter Value A ")
+    val x: Int = readLine()!!.toInt()
+    println("Enter Value B ")
+    val y: Int = readLine()!!.toInt()
+    println("Enter Value C ")
+    val z: Int= readLine()!!.toInt()
+    val sum: Int =x+y+z
+    val SUB: Int =x-y-z
+    val MUL: Int =x*y*z
+    val DIV: Int =x/y/z
+    println(" Addition $x + $y +$z = $sum")
+    println(" Subtraction $x - $y - $z = $SUB")
+    println(" Multiplication $x * $y * $z = $MUL")
+    println(" Division $x / $y / $z = $DIV")
+
+    println("********** Practical = 1_7 **********")
+    println("Enter no to find Factorial = ")
+    val No: Int = readLine()!!.toInt()
+    println("Factorial of $No is: "+Fact(No))
+
+    println("********** Practical = 1_8 **********")
+    println("********** Practical = 1_9 **********")
 
 }
