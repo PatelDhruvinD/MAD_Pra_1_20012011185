4f8a05b2368ed40791be70d72a1dda8038f6ab5d
Malay Thakkar <78149426+Malay-Thakkar@users.noreply.github.com>
2022-08-25 18:44:20 +0530

    Update Practical_1_exercises.kt

--- src/main/kotlin/Practical_1_exercises.kt
+++ src/main/kotlin/Practical_1_exercises.kt
@@ -1,4 +1,5 @@
 fun main(){
+    println("********** Exercises = 1_1 **********")
     var a=10
     var b=20
     var c=a
@@ -24,8 +25,7 @@ fun main(){
    
    
    
-
-    println("20012011169_MALAY THAKKAR\n")
+    println("********** Exercises = 1_2 **********") 
 
     val obj = ArrayList<Laptop> (6)
 
@@ -42,7 +42,7 @@ fun main(){
         println("----------------------------")
     }
 
-}
+
 open class Product(private var name: String, private var qty: Int, private var amt: Int)
 {
 
@@ -74,5 +74,47 @@ class Laptop(
         println("HDD Size : $hdd GB")
     }
 }
+println("********** Exercises = 1_3 **********") 
+    val obj = ArrayList<Student> (6)
 
+    obj.add(Student())
+    obj.add(Student("30002001001","CE","C","AB7","Rahul","Agarwal",19))
+    obj.add(Student("T20012021050","IT","D","AB14","Om","Patel", 22))
+    obj.add(Student("20112022222","CE","A","AB2","Chirag","Patel",19))
+    obj.add(Student("20012022123","IT","D","AB5","Droan","Acharya", 18))
+    obj.add(Student("21112022010","CE-AI","B","AB6","Rohan","Patel",17))
+
+    for (i in obj)
+    {
+        i.details()
+        i.studentinfo()
+        println("----------------------------")
+    }
+
+
+open class Person(private val fname: String, private val lname: String, private val age: Int)
+{
+    fun details() {
+        println("First Name = $fname")
+        println("Last Name = $lname")
+        println("Age = $age")
+    }
+
+
+class Student(private val enroll: String,private val branch: String,private val Class: String,private val batch: String,fname: String,lname: String,age: Int) : Person(fname, lname, age)
+{
+
+    constructor() : this("NaN", "NaN", "NaN", "NaN", "NaN", "NaN", 0)
+    {
+        println("Secondary Constructor")
+    }
+
+    fun studentinfo()
+    {
+        println("Enrollment Number : $enroll")
+        println("Branch : $branch")
+        println("Class : $Class")
+        println("Batch : $batch")
+    }
+}
 }
